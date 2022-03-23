68677b51527d840ffe31fe483c119338c8fd32ef
Saki 09 <75006749+saki-saki09@users.noreply.github.com>
2022-03-23 23:19:11 +0600

    Update file-deleter.py

--- file-deleter.py
+++ file-deleter.py
@@ -5,3 +5,10 @@ if os.path.exists(file):
   print("\n'%s' file successfully deleted!" % file)
 else:
   print("\n'%s' file doesn't exist..." % file)
+
+"""
+This is an trial file for Python Practice... By using this... when you run this file to your terminal... 
+You have to write your file name and press "Enter"... By pressing "Enter" the file will be deleted...
+
+Θ But here's a CONDITION that the file that you want to delete by using this 'file-deleter.py', must be exist at the same folder Θ
+"""
