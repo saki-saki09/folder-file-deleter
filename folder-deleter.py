7fdd9b2b9a88374212e835bcd88ff9a24b5e56c3
Saki 09 <75006749+saki-saki09@users.noreply.github.com>
2022-03-23 23:23:12 +0600

    Update folder-deleter.py

--- folder-deleter.py
+++ folder-deleter.py
@@ -5,3 +5,10 @@ if os.path.exists(folder):
   print("\n'%s' folder successfully deleted!" % folder)
 else:
   print("\n'%s' folder doesn't exist..." % folder)
+
+"""
+This is a trial file for Python Practice... By using this... when you run this file to your terminal... 
+You have to write your folder name and press "Enter"... By pressing "Enter" the folder will be deleted...
+
+Θ But here's a CONDITION that the folder that you want to delete by using this 'folder-deleter.py', must exist at the same folder Θ
+"""
