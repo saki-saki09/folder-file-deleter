e0cd50caec11f02008f1dc7cae593c41d29c5ba3
Saki 09 <75006749+saki-saki09@users.noreply.github.com>
2022-03-24 00:09:18 +0600

    Update folder-deleter.py

--- folder-deleter.py
+++ folder-deleter.py
@@ -1,5 +1,5 @@
 import os
-folder = input("Which file do you want to delete?\n")
+folder = input("Which folder do you want to delete?\n")
 if os.path.exists(folder):
   os.rmdir(folder)
   print("\n'%s' folder successfully deleted!" % folder)
